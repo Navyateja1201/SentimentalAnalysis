30bf710fe0773f6d2f741fa43144236b3145923c
Navya Teja <137189073+Navyateja1201@users.noreply.github.com>
2024-03-24 13:13:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+####################################################
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/prediction', methods=['get', 'post'])
+def prediction():
+    user_review = request.form.get("test_string")
+
+    model = joblib.load("model/random_forest.pkl")
+
+    # Predict the sentiment
+    prediction = model.predict([user_review])
+    
+    # Interpret the prediction
+    result = "Positive" if prediction[0] == 1 else "Negative"
+
+    return render_template("output.html", prediction=result)
+
+######################################################
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
